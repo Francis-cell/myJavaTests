08ef7b2d191233caf3d563f88cec01dd8253cce1
zhumengren <franciszmr@foxmail.com>
2022-09-17 14:17:01 +0800

    1、LeetCode刷题更新9.13-9.14

---
+++ src/javaTests/MyTest17.java
@@ -0,0 +1,31 @@
+package javaTests;
+
+import java.text.ParseException;
+
+/**
+ * @ClassName MyTest12
+ * @Description int数据转换成String类型，然后获取长度
+ * @Author zhumengren
+ * @Date 2022/6/7 10:46
+ * @Email zhumengren@sinosoft.com
+ * @Version 1.0
+ **/
+public class MyTest17 {
+    public static void main(String[] args) throws ParseException {
+        //int a = 1234567;
+        //// String.valueOf(a)
+        //// Integer.toString(a)
+        ////System.out.println(Integer.toString(a).length());
+        //System.out.println(a % 10);
+        //ArrayList arrayList = new ArrayList();
+        //arrayList.add("1");
+        //arrayList.add("2");
+        //arrayList.add("3");
+        //arrayList.add("4");
+        //arrayList.set(0, "a");
+        //System.out.println(arrayList);
+
+        String str = "123456789";
+        System.out.println(str.length());
+    }
+}
--- src/sortArithmetic/SelectSorted.java
+++ src/sortArithmetic/SelectSorted.java
@@ -1,32 +1,32 @@
-package sortArithmetic;
-
-import static myUtils.ArrayUtils.printArray;
-
-/**
- * @author 朱梦仁 franciszmr@foxmail.com
- * @version 1.0
- * @date 2021/9/25 20:50
- */
-public class SelectSorted {
-
-    /**
-     * 选择排序
-     * @param arr
-     */
-    public static void select_sorted(int arr[]){
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            if (minIndex != i){
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
-            }
-        }
-        printArray(arr);
-    }
-}
+//package sortArithmetic;
+//
+//import static myUtils.ArrayUtils.printArray;
+//
+///**
+// * @author 朱梦仁 franciszmr@foxmail.com
+// * @version 1.0
+// * @date 2021/9/25 20:50
+// */
+//public class SelectSorted {
+//
+//    /**
+//     * 选择排序
+//     * @param arr
+//     */
+//    public static void select_sorted(int arr[]){
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            int minIndex = i;
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[j] < arr[minIndex]) {
+//                    minIndex = j;
+//                }
+//            }
+//            if (minIndex != i){
+//                int temp = arr[i];
+//                arr[i] = arr[minIndex];
+//                arr[minIndex] = temp;
+//            }
+//        }
+//        printArray(arr);
+//    }
+//}
