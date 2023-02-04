2cbbab3f4792d51870eb4a16df06ef86afaceba8
zhumengren <franciszmr@foxmail.com>
2023-02-04 21:58:07 +0800

    [add]JVM调优-GC学习

---
+++ src/LearningFiles/MyJVMTests/Garbage/HelloGC.java
@@ -0,0 +1,20 @@
+package LearningFiles.MyJVMTests.Garbage;
+
+/**
+ * @author 朱梦仁 franciszmr@foxmail.com
+ * @version 1.0
+ * @date 2023/2/4 11:11
+ */
+import java.util.List;
+import java.util.LinkedList;
+public class HelloGC {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        System.out.println("HelloGc!");
+        List list = new LinkedList();
+        for(;;) {
+            byte[] b = new byte[1024*1024];
+            list.add(b);
+        }
+    }
+}
