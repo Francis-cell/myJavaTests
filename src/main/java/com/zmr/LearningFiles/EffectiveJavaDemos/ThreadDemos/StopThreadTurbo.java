a9e5503f31b2af9fbc5bea8551a73171e625d1e4
zmr <zhumengren@sinosoft.com.cn>
2024-01-14 17:20:41 +0800

    Effective java 学习

---
+++ src/main/java/com/zmr/LearningFiles/EffectiveJavaDemos/ThreadDemos/StopThreadTurbo.java
@@ -0,0 +1,33 @@
+package com.zmr.LearningFiles.EffectiveJavaDemos.ThreadDemos;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 注意写和读操作都被同步了
+ * 只有读被同步 or 写被同步是不够的，需要两者都被同步，才能保证同步能够起作用
+ */
+public class StopThreadTurbo {
+    private static boolean stopRequested;
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopRequested;
+    }
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+           int i = 0;
+           while (!stopRequested()) {
+               i++;
+           }
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        requestStop();
+    }
+}
---
+++ src/main/java/com/zmr/LearningFiles/EffectiveJavaDemos/ThreadDemos/SynchronizedMethod.java
@@ -0,0 +1,7 @@
+package com.zmr.LearningFiles.EffectiveJavaDemos.ThreadDemos;
+
+public class SynchronizedMethod {
+    public static void main(String[] args) {
+
+    }
+}
