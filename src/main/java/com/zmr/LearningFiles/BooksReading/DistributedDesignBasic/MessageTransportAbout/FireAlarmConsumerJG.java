434cc72659e55eb2fbf0b629fbfae2bf5719ad8c
franciszmr <franciszmr@foxmail.com>
2024-11-20 23:01:23 +0800

    LeetCode 刷题提交

--- src/main/java/com/zmr/LearningFiles/BooksReading/DistributedDesignBasic/MessageTransportAbout/FireAlarmConsumerJG.java
+++ src/main/java/com/zmr/LearningFiles/BooksReading/DistributedDesignBasic/MessageTransportAbout/FireAlarmConsumerJG.java
@@ -1,64 +1,64 @@
-package com.zmr.LearningFiles.BooksReading.DistributedDesignBasic.MessageTransportAbout;
-
-import org.jgroups.JChannel;
-import org.jgroups.Message;
-import org.jgroups.ReceiverAdapter;
-import org.jgroups.View;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class FireAlarmConsumerJG extends ReceiverAdapter {
-    private JChannel channel;
-    private final BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
-
-    public void start() throws Exception {
-        channel = new JChannel();
-        channel.setReceiver(this);
-        channel.connect("AlarmChannel");
-        System.out.println("FireAlarmConsumerJG started and connected to channel.");
-    }
-
-    @Override
-    public void receive(Message msg) {
-        String message = (String) msg.getObject();
-        System.out.println("Received message: " + message);
-        try {
-            messageQueue.put(message);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void viewAccepted(View newView) {
-        System.out.println("** view: " + newView);
-    }
-
-    public void stop() {
-        if (channel != null) {
-            channel.close();
-        }
-    }
-
-    public String await() {
-        try {
-            return messageQueue.take();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        FireAlarmConsumerJG consumer = new FireAlarmConsumerJG();
-        consumer.start();
-        // Keep the application running to receive messages
-        Thread.sleep(1000000);
-        consumer.stop();
-        // String msg = consumer.await();
-        // System.out.println("Alarm received: " + msg);
-    }
-}
+//package com.zmr.LearningFiles.BooksReading.DistributedDesignBasic.MessageTransportAbout;
+//
+//import org.jgroups.JChannel;
+//import org.jgroups.Message;
+//import org.jgroups.ReceiverAdapter;
+//import org.jgroups.View;
+//
+//import java.util.concurrent.BlockingQueue;
+//import java.util.concurrent.LinkedBlockingQueue;
+//
+//public class FireAlarmConsumerJG extends ReceiverAdapter {
+//    private JChannel channel;
+//    private final BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
+//
+//    public void start() throws Exception {
+//        channel = new JChannel();
+//        channel.setReceiver(this);
+//        channel.connect("AlarmChannel");
+//        System.out.println("FireAlarmConsumerJG started and connected to channel.");
+//    }
+//
+//    @Override
+//    public void receive(Message msg) {
+//        String message = (String) msg.getObject();
+//        System.out.println("Received message: " + message);
+//        try {
+//            messageQueue.put(message);
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Override
+//    public void viewAccepted(View newView) {
+//        System.out.println("** view: " + newView);
+//    }
+//
+//    public void stop() {
+//        if (channel != null) {
+//            channel.close();
+//        }
+//    }
+//
+//    public String await() {
+//        try {
+//            return messageQueue.take();
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        FireAlarmConsumerJG consumer = new FireAlarmConsumerJG();
+//        consumer.start();
+//        // Keep the application running to receive messages
+//        Thread.sleep(1000000);
+//        consumer.stop();
+//        // String msg = consumer.await();
+//        // System.out.println("Alarm received: " + msg);
+//    }
+//}
--- src/main/java/com/zmr/LearningFiles/OwnLearning/MyAlgorithmTests/LeetCodePractice/LeetCode100/MoveZeroes.java
+++ src/main/java/com/zmr/LearningFiles/OwnLearning/MyAlgorithmTests/LeetCodePractice/LeetCode100/MoveZeroes.java
@@ -13,89 +13,45 @@ public class MoveZeroes {
      * @param nums
      */
     public static void moveZeroesCrude(int[] nums) {
-        // // O(2n) 时间复杂度
-        // // 1、先遍历一遍数组，找到 0 的总数，并记录下 0 的下标列表
-        // // 2、遍历移动
-        // // 3、如果 zeroIndexList.get(zeroIndexList.size() - 1) 不是最后一个元素，则说明后面的元素需要向前移动
-        // // 4、末尾赋值 0
-        //
-        // // 1、先遍历一遍数组，找到 0 的总数，并记录下 0 的下标数组
-        // ArrayList<Integer> zeroIndexList = new ArrayList<>();
-        // // 用于移动完元素之后末尾赋值 0 操作使用
-        // int zeroCount = 0;
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (nums[i] == 0) {
-        //         zeroIndexList.add(i);
-        //         zeroCount++;
-        //     }
-        // }
-        //
-        // // 2、遍历移动（两个 0 元素之间的元素统一移动）
-        // for (int i = 0; i < zeroIndexList.size() - 1; i++) {
-        //     // 开始移动元素下标
-        //     int startMoveIndex = zeroIndexList.get(i);
-        //     // 结束移动元素下标
-        //     int stopMoveIndex = zeroIndexList.get(i + 1);
-        //     // 刚开始不会存在两个 0
-        //     if (i != 0) {
-        //         // 末尾出现两个空格，因此下标需要维护成新的，+2 操作
-        //         startMoveIndex += 2;
-        //         stopMoveIndex += 2;
-        //         // 执行移动
-        //         for (int j = startMoveIndex + 1; j < stopMoveIndex; j++) {
-        //             nums[j - 2] = nums[j];
-        //         }
-        //     } else {
-        //         // 执行移动
-        //         for (int j = startMoveIndex + 1; j < stopMoveIndex; j++) {
-        //             nums[j - 1] = nums[j];
-        //         }
-        //     }
-        // }
-        //
-        // // 3、如果 zeroIndexList.get(zeroIndexList.size() - 1) 不是最后一个元素，则说明后面的元素需要向前移动
-        // int endStartMoveIndex = zeroIndexList.get(zeroIndexList.size() - 1);
-        // int endStopMoveIndex = nums.length;
-        // if (zeroIndexList.get(zeroIndexList.size() - 1) != nums.length) {
-        //     // 执行移动
-        //     for (int i = endStartMoveIndex + 1; i < endStopMoveIndex; i++) {
-        //         nums[i - 1] = nums[i];
-        //     }
-        // }
-        //
-        // // 4、末尾赋值 0
-        // for (int i = nums.length - 1; i < zeroCount; i--) {
-        //     nums[i] = 0;
-        // }
-    }
-
-    /**
-     * <p> 元素交换求解法 </p>
-     * @param nums
-     */
-    public static void moveZeroesCrudeNew(int[] nums) {
-        // 0 元素序列之后的第一个不为 0 的元素的下标
-        int notZeroIndex = 0;
-        // 当前处理到的为 0 的元素下标
-        int zeroIndex = 0;
-        for (int i = 0; i < nums.length ; i++) {
-            // 找到当前情况下第一个不为 0 的元素和第一个为 0 的元素的下标值
-            // 如果 nums[zeroIndex] 已经是 0 了则不进行交换
-            // zeroIndex == -1 的时候，代表第一次进行赋值
-            if (nums[i] == 0 && (zeroIndex == 0 || nums[zeroIndex] != 0)) {
-                zeroIndex = i;
-            } else {
-                notZeroIndex = i;
-            }
-
-            // 执行元素交换
-            if (zeroIndex != notZeroIndex && nums[notZeroIndex] != 0) {
-                swapElementForArr(nums, zeroIndex, notZeroIndex);
-                zeroIndex++;
-                notZeroIndex = i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        swapElementForArr(nums, i, j);
+                        break;
+                    }
+                }
             }
         }
     }
+
+    ///**
+    // * <p> 元素交换求解法 </p>
+    // * @param nums
+    // */
+    //public static void moveZeroesCrudeNew(int[] nums) {
+    //    // 0 元素序列之后的第一个不为 0 的元素的下标
+    //    int notZeroIndex = 0;
+    //    // 当前处理到的为 0 的元素下标
+    //    int zeroIndex = 0;
+    //    for (int i = 0; i < nums.length ; i++) {
+    //        // 找到当前情况下第一个不为 0 的元素和第一个为 0 的元素的下标值
+    //        // 如果 nums[zeroIndex] 已经是 0 了则不进行交换
+    //        // zeroIndex == -1 的时候，代表第一次进行赋值
+    //        if (nums[i] == 0 && (zeroIndex == 0 || nums[zeroIndex] != 0)) {
+    //            zeroIndex = i;
+    //        } else {
+    //            notZeroIndex = i;
+    //        }
+    //
+    //        // 执行元素交换
+    //        if (zeroIndex != notZeroIndex && nums[notZeroIndex] != 0) {
+    //            swapElementForArr(nums, zeroIndex, notZeroIndex);
+    //            zeroIndex++;
+    //            notZeroIndex = i;
+    //        }
+    //    }
+    //}
 
     /**
      * <p> 交换 nums 数组中的 index1 和 index2 下标的元素值 </p>
@@ -115,6 +71,28 @@ public class MoveZeroes {
         nums[index2] = swapElement;
     }
 
+
+    /**
+     * <p> 双指针解法 </p>
+     * @param nums
+     */
+    public static void moveZeroes(int[] nums) {
+        // 定义第一个非 0 的元素的指针的位置
+        int cur = 0;
+        // 开始收集不是 0 的数
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[cur++] = nums[i];
+            }
+        }
+
+        // 将剩余位置的元素赋值为 0
+        for (int i = cur; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+
     public static void main(String[] args) {
         // 1,3,12,0,0
         int[] nums1 = new int[] {0,1,0,3,12};
@@ -123,11 +101,11 @@ public class MoveZeroes {
         // 1,0
         int[] nums3 = new int[] {1, 0};
 
-        // moveZeroesCrudeNew(nums1);
-        // PrintUtilsImpl.getInstance().printArr(nums1);
-        // moveZeroesCrude(nums2);
-        // PrintUtilsImpl.getInstance().printArr(nums2);
-        moveZeroesCrudeNew(nums3);
-        PrintUtilsImpl.getInstance().printArr(nums3);
+        moveZeroes(nums1);
+        PrintUtilsImpl.getInstance().printArr(nums1);
+        //moveZeroesCrude(nums2);
+        //PrintUtilsImpl.getInstance().printArr(nums2);
+        //moveZeroesCrude(nums3);
+        //PrintUtilsImpl.getInstance().printArr(nums3);
     }
 }
